63851d1a3e0799f6e770736aba2e908272004b25
Nguyễn Đình Nam <123957400+nguyendihnam@users.noreply.github.com>
2023-04-01 14:58:45 +0700

    Update

--- coffee-shop-website-design-main/ProductDetails.php
+++ coffee-shop-website-design-main/ProductDetails.php
@@ -1,33 +1,56 @@
 <?php
+   include_once 'DBconnect.php';
+
+   # Get code
+   if(isset($_GET['id'])):
+      header("location:menu.php");
+   endif;
+   $id = $_GET['id'];
+   #4. Excute query
+   $query = "SELECT * FROM product WHERE ID='{$id}'";
+   $rs = mysqli_query($conn, $query);
+   $count = mysqli_num_rows($rs);
+   $fields = mysqli_fetch_array($rs);
+   
+?>
+<?php
     include'./header.php';
     include'./slider.php';  
 ?>
-<?php
-     #1. Start session
-     session_start();
-     #2. Connect to database
-     include_once 'DBconnect.php';
-     #3. Execute query
-?>
-  <Form>
-        <div class="menu">
-            <div class="card-container" class="display_flex">
-                <div class="card">
-                    <a href="./image/" class="card-thumbnail">
-                        <img src="./image/" alt="" class="img-thumbnail">
-                    </a>
-                    <div class="card-text-container">
-                       <a class="card-item">
-                            <h2 class="card-name"></h2>
-                        </a>
-                            <p class="card-price">$</p>
-                            <p class="card-description"></p>
-                        <a href="./shoppingcart.php" class="card-btn btn">Add to cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-  </Form>
+
+  <form action="order.php" method="post">
+  <?php
+            if ($count == 0):
+                echo 'Records not founds';
+            else:
+                while ($fields = mysqli_fetch_array($rs)):
+                    ?>
+  <div>
+    <img src="./image/<?= $fields[0] ?>" alt="Product Image">
+  </div>
+  <div>
+    <h2><?= $fields[0] ?></h2>
+    <p>Price: $100</p>
+    <p>
+      <label>Size:</label>
+      <select name="size">
+        <option value="S">S</option>
+        <option value="M">M</option>
+        <option value="L">L</option>
+      </select>
+    </p>
+    <p>
+      <label>Quantity:</label>
+      <input type="number" name="quantity" min="1" max="10">
+    </p>
+    <button type="submit" name="order">Order Now</button>
+  </div>
+  <?php
+  endwhile;
+endif;
+  ?>
+</form>
+
 
    <?php
         include'./footer.php';
--- coffee-shop-website-design-main/menu.php
+++ coffee-shop-website-design-main/menu.php
@@ -1,18 +1,28 @@
 <?php
-     #1. Start session
-     session_start();
-     #2. Connect to database
-     include_once 'DBconnect.php';
-     #3. Execute query
+    #1. Start session
+session_start();
+#2. Connect to database
+include_once 'DBconnect.php';
+#3. Execute query
 
-    $queryCategory = "select * from category ";
-    $rsCategory = mysqli_query($conn, $queryCategory);
-    $count = mysqli_num_rows($rsCategory);
+$queryCategory = "SELECT * FROM category ";
+$rsCategory = mysqli_query($conn, $queryCategory);
+$countCategory = mysqli_num_rows($rsCategory);
 
-    $queryProduct = "select * from product ";
-    $rsProduct = mysqli_query($conn, $queryProduct);
-    $count = mysqli_num_rows($rsProduct);
-?>
+$queryProduct = "SELECT * FROM product ";
+$rsProduct = mysqli_query($conn, $queryProduct);
+$countProduct = mysqli_num_rows($rsProduct);
+
+if(isset($_GET['btnSearch'])):
+   $search = $_GET['txtSearch'];
+   $queryProduct = "SELECT * FROM product WHERE Name LIKE '%{$search}%'";
+   $rsProduct = mysqli_query($conn, $queryProduct);
+   $countProduct = mysqli_num_rows($rsProduct);
+endif;
+
+
+$count = mysqli_num_rows($rsCategory); // Define $count here
+        ?>
 <?php
     include'./header.php';
     include'./slider.php';  
@@ -40,8 +50,8 @@
             endif;
             ?>
             </nav>
-        
-            <div class="card-container">
+
+        </div>  
             <?php
             if ($count == 0):
                 echo 'Records not founds';
