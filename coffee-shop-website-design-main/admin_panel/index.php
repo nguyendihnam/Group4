9adb350de6639e150f904eae7118ef28a522841d
HuyTrLe <htranle1@gmail.com>
2023-04-13 00:18:04 +0700

    link contact-modal.js

--- coffee-shop-website-design-main/admin_panel/index.php
+++ coffee-shop-website-design-main/admin_panel/index.php
@@ -87,6 +87,7 @@
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+	<script src="./assets/js/contact-modal.js"></script>
 </body>
  
 </html>
